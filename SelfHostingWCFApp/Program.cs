5fd4b3fc4a90e2048d88a58b7bd68b9d4706abb7
hvidgaard <peter@hvidgaard.dk>
2014-02-10 11:58:34 +0100

    WCFService can run in IIS or selfhosted.

--- SelfHostingWCFApp/Program.cs
+++ SelfHostingWCFApp/Program.cs
@@ -13,31 +13,17 @@ namespace SelfHostingWCFApp
     {
         static void Main(string[] args)
         {
-            Uri baseAddress = new Uri("http://localhost:80/hello/");
-
-            // Create the ServiceHost.
-            using (ServiceHost host = new ServiceHost(typeof(Service1), baseAddress))
+            using (ServiceHost host = new ServiceHost(typeof(Service1)))
             {
-                // Enable metadata publishing.
-                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
-                smb.HttpGetEnabled = true;
-                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
-                host.Description.Behaviors.Add(smb);
-
-                // Open the ServiceHost to start listening for messages. Since
-                // no endpoints are explicitly configured, the runtime will create
-                // one endpoint per base address for each service contract implemented
-                // by the service.
                 host.Open();
 
-                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("The service is ready at {0}");
                 Console.WriteLine("Press <Enter> to stop the service.");
                 Console.ReadLine();
 
-                // Close the ServiceHost.
                 host.Close();
             }
-
+            Console.ReadLine();
         }
     }
 }
